3781218a2d68a03a95a9ba9b1bc261b7e3747727
Luan M Paes <luanmp25@gmail.com>
2025-12-18 13:10:43 -0300

    H2 Database

---
+++ backend/src/main/java/com/devsuperior/DsCatalog/resources/CategoryResource.java
@@ -0,0 +1,28 @@
+package com.devsuperior.DsCatalog.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.DsCatalog.entities.Category;
+import com.devsuperior.DsCatalog.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@Autowired
+	private CategoryService service;
+
+	@GetMapping
+	public ResponseEntity<List<Category>>findAll(){
+		List<Category>list = service.findAll();
+
+		return ResponseEntity.ok().body(list);
+	}
+}
+
